68f465e21eecc4dfecf0381fdb13050d33c4cfce
Ednilton Martins da Silva <ednilton17@gmail.com>
2024-02-11 19:49:03 -0300

    Adding Mux Gorilla routes

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/edniltonms17/api-go-rest/models"
+	"github.com/gorilla/mux"
 )
 
 const homepage = "Home Page"
@@ -17,4 +19,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 func AllPersonalities(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.Personalities)
 
+}
+
+func PersonalityById(w http.ResponseWriter, r *http.Request) {
+	personalities := mux.Vars(r)
+	id := personalities["id"]
+	for _, personality := range models.Personalities {
+		if strconv.Itoa(personality.Id) == id {
+			json.NewEncoder(w).Encode(personality)
+		}
+	}
 }
