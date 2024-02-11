e8ee77ab2905d3f44d7d1d8cf1eab057764c9060
Ednilton Martins da Silva <ednilton17@gmail.com>
2024-02-11 20:24:40 -0300

    Fixed in the files about Methods

--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,6 @@ func HandleRequest() {
 	route := mux.NewRouter()
 	route.HandleFunc("/", controller.Home)
 	route.HandleFunc("/api/personalities", controller.AllPersonalities).Methods("Get")
-	route.HandleFunc("/api/personalities/{id}", controller.PersonalityById)
+	route.HandleFunc("/api/personalities/{id}", controller.PersonalityById).Methods("Get")
 	log.Fatal(http.ListenAndServe(":8000", route))
 }
