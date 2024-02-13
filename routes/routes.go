c36b4e72c5be3d08a5f0550ffca6d319f69af064
Ednilton Martins da Silva <ednilton17@gmail.com>
2024-02-12 21:53:33 -0300

    Adding content-type using Middleware

--- routes/routes.go
+++ routes/routes.go
@@ -5,16 +5,20 @@ import (
 	"net/http"
 
 	controller "github.com/edniltonms17/api-go-rest/controllers"
+	"github.com/edniltonms17/api-go-rest/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	route := mux.NewRouter()
+	route.Use(middleware.ContentTypeMiddleware)
 	route.HandleFunc("/", controller.Home)
-	route.HandleFunc("/api/personalities", controller.AllPersonalities).Methods("Get")
-	route.HandleFunc("/api/personalities/{id}", controller.PersonalityById).Methods("Get")
-	route.HandleFunc("/api/personalities", controller.CreatePersonality).Methods("Post")
-	route.HandleFunc("/api/personalities/{id}", controller.DeletePersonality).Methods("Delete")
-	route.HandleFunc("/api/personalities", controller.EditPersonality).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", route))
+	const personalitiesURL string = "/api/personalities"
+	route.HandleFunc(personalitiesURL, controller.AllPersonalities).Methods("Get")
+	route.HandleFunc(personalitiesURL+"/{id}", controller.PersonalityById).Methods("Get")
+	route.HandleFunc(personalitiesURL, controller.CreatePersonality).Methods("Post")
+	route.HandleFunc(personalitiesURL+"/{id}", controller.DeletePersonality).Methods("Delete")
+	route.HandleFunc(personalitiesURL, controller.EditPersonality).Methods("Put")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(route)))
 }
